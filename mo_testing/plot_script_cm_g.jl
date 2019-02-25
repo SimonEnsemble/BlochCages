7b2227ad8f0aa215ec5ba0b800c43a2a755bda01
Arthur York <ahyork98@gmail.com>
2019-02-24 16:30:35 -0800

    added data for cm/g and made new plotting file for it

--- mo_testing/plot_script_cm_g.jl
+++ mo_testing/plot_script_cm_g.jl
@@ -16,20 +16,20 @@ input_file = ARGS[1]
 mmolg = [results[i]["⟨N⟩ (mmol/g)"] for i = 1:length(results)]
 
 # Converted mmol/g -> cm^3 STP/cm^3
-# (mmol) * (22.4 L STP) * (density kg) * (1000 g) * (  m^3 ) * (    L     * 1000 cm^3) = (22.4 * density) (L STP)
-# ( g  )   (1000 mmol )   (   m^3    )   (  kg  )   (1000 L)   (1000 cm^3 *     L    )   (     1000     ) (  L  )
+# (mmol) * (22.4 L STP) * (1000 cm^3) = (22.4) (cm^3 STP)
+# ( g  )   (1000 mmol )   (    L    )          (    g   )
 
-vstpv = mmolg .* (22.4 * density / 1000)
+vstpg = mmolg .* (22.4)
 pressures = [results[i]["pressure (bar)"] for i = 1:length(results)]
 
 df = CSV.File(ARGS[2]) |> DataFrame
 
 grid(true, linestyle="--", zorder=0) # the grid will be present
 #set_axisbelow(true)
-plot(pressures, vstpv, label="Simulated " * ARGS[3], color=:blue, marker="o", zorder=1000) # simulated data
+plot(pressures, vstpg, label="Simulated " * ARGS[3], color=:blue, marker="o", zorder=1000) # simulated data
 scatter(df[1], df[2], label="Experimental " * ARGS[3], color=:orange, marker="^", zorder=1000)
 xlabel("Pressure (bar)")
-ylabel(L"Methane Adsorbed (cm$^3$/cm$^3$)") 
+ylabel(L"Methane Adsorbed (cm$^3$/g)") 
 title("Adsorption Isotherm for " * ARGS[3]) # plot is labelled based on structure name
 legend(loc=4) # legend will display in the lower right
 
