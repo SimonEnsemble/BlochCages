badc3f5f8e5a42915ebd1c0e57b95499cc2b4dea
Arthur York <yorkar@oregonstate.edu>
2019-02-27 19:20:03 -0800

    changed name of job running on the serve

--- structure_testing/structure_script.sh
+++ structure_testing/structure_script.sh
@@ -5,11 +5,11 @@
 #$ -cwd
 
 # name this job
-#$ -N mo_testing
+#$ -N mo_co_testing
 
 # send stdout and stderror to this file
-#$ -o mo_testing.o
-#$ -e mo_testing.e
+#$ -o mo_co_testing.o
+#$ -e mo_co_testing.e
 #$ -j y
 
 #the list of users who will recieve mail about this job
